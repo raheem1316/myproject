0bbfd4127349db0627f118808cfb1c0dbcfdcf40
raheem1316 <34540315+raheem1316@users.noreply.github.com>
2018-01-18 11:04:20 +0530

    Update admin.py

--- queriesapp/admin.py
+++ queriesapp/admin.py
@@ -3,4 +3,19 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from queriesapp.models import Seller_details, Product_details, Orders
+
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ('name','number','address','date')
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name','product_quantity','product_price','seller')
+
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('order_name','order_quantity','seller_name','buyer_name','buyer_address','order_date')
+
+
+
+admin.site.register(Orders,OrdersAdmin)
+admin.site.register(Seller_details,SellerAdmin)
+admin.site.register(Product_details,ProductAdmin)
